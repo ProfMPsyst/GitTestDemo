9f350907f6c013a3ba2b56d4367d1fdca98f5cdd
ProfMPsyst <65903834+ProfMPsyst@users.noreply.github.com>
2026-01-26 17:46:55 -0500

    function added

--- src/main/java/test/test/Hello.java
+++ src/main/java/test/test/Hello.java
@@ -3,10 +3,15 @@ package test.test;
 
 /**
  *
- * @author Megha Patel
+ * @author Megha Patel, January 2026
  */
 public class Hello {
     public String sayHello(){
         return "Hello World!!!";
+    }
+
+    public double addNumbers(double a, double b){
+
+        return a+b;
     }
 }
